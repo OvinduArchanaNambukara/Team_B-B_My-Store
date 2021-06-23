a395625209dcfa526dbf9a1f3e4a51104e049f59
Sriyanjith Herath <sriyabro@gmail.com>
2021-06-24 04:15:21 +0530

    Footer copyrights added

--- src/components/footer/FooterCopyrights.tsx
+++ src/components/footer/FooterCopyrights.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {Row} from "react-bootstrap";
 
 const FooterCopyrights: React.FC = () => {
   return (
-    <></>
+    <Row className="justify-content-center">
+      Copyright &copy; 2021. All Rights Reserved.&nbsp;<span className="powered">Powered by SoftVessel</span>
+    </Row>
   );
 }
 
