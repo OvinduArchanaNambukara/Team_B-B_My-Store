021755012f56776ae09b9be1f5a65fb45ed587e5
Sriyanjith Herath <sriyabro@gmail.com>
2021-06-24 04:06:55 +0530

    Footer links added

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import {Col, Row} from "react-bootstrap";
 
 const FooterLinks: React.FC = () => {
   return (
-    <></>
+    <Row className="justify-content-center">
+      <Col xs={12} md="auto">ABOUT US</Col>
+      <Col xs={12} md="auto">FAQ</Col>
+      <Col xs={12} md="auto">DELIVERY INFO</Col>
+      <Col xs={12} md="auto">CONTACT US</Col>
+    </Row>
   );
 }
 
