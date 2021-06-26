786c2e96de3d2bd07eeb3341618fed7a8bf43f35
ovindu <ovindunambukara@gmail.com>
2021-06-26 16:00:32 +0530

    add search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+import Select from "react-select/base";
+
+const SearchBar: React.FC = () => {
+    return (
+        <Row>
+            <Col xs={12}>
+                <Select
+                />
+            </Col>
+        </Row>
+    );
+}
+
+export default SearchBar;
