c7dde825ec22cc09743f6f21a8a7b9ecce12329f
ovindu <ovindunambukara@gmail.com>
2021-06-27 02:32:37 +0530

    add search bar and styled it

--- src/Client.tsx
+++ src/Client.tsx
@@ -3,16 +3,15 @@ import {Container} from "react-bootstrap";
 import CategoryArea from "./components/CategoryArea";
 import Welcome from "./components/Welcome";
 import SearchBar from "./components/SearchBar";
-
 import Footer from "./components/Footer";
 
 const Client: React.FC = () => {
     return (
         <Container fluid={true}>
             <Welcome/>
-            <CategoryArea/>
             <SearchBar/>
-            <Footer />
+            <CategoryArea/>
+            <Footer/>
         </Container>
     );
 }
