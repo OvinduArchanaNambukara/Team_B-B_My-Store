0cb0df933350b5ddbfbc8abcf71021af2b72bb9b
Sriyanjith Herath <sriyabro@gmail.com>
2021-06-28 19:26:46 +0530

    fixed product image sizes refactored variable names

--- src/Client.tsx
+++ src/Client.tsx
@@ -5,7 +5,7 @@ import Welcome from "./components/welcome/Welcome";
 import SearchBar from "./components/search-bar/SearchBar";
 import Footer from "./components/footer/Footer";
 import Products from "./components/ProductUX/Products";
-import BottomNav from "./components/BottomNav";
+import BottomNav from "./components/bottom-nav/BottomNav";
 
 const Client: React.FC = () => {
     return (
