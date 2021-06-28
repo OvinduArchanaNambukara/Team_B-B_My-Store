d7dd3d8c683fad3f370085a6d0332e9d164ec2cb
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-06-28 16:26:31 +0530

    Add product images

--- src/components/ProductUX/Quantity.tsx
+++ src/components/ProductUX/Quantity.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Col, FormControl, InputGroup} from "react-bootstrap";
 
 const Quantity: React.FC = () => {
+
+    const [unit, setUnit] = useState<boolean>(true);
+
+    const handleChangeUnit = () => {
+        setUnit(!unit);
+    }
+
     return (
         <Col xs={12} md={6} className='pr-md-0 pl-md-3 px-4'>
             <InputGroup className="mb-3">
                 <FormControl
                     type="number"
+                    min="1"
                 />
-                <InputGroup.Text id="basic-addon1">Kg</InputGroup.Text>
+                <InputGroup.Text id="basic-addon1" onClick={() => handleChangeUnit()} >{unit ? "kg": "g"}</InputGroup.Text>
             </InputGroup>
         </Col>
     )
