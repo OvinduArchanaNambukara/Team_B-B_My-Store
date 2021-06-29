7b5d11313545b7cfdc29d3b7a24be2ce2807b575
ovindu <ovindunambukara@gmail.com>
2021-06-29 15:48:29 +0530

    responsibility for xs,md,lg

--- src/components/top-nav-bar/HeaderArea.tsx
+++ src/components/top-nav-bar/HeaderArea.tsx
@@ -4,8 +4,8 @@ import {IoCallSharp, FaTruck, FaUserCircle} from "react-icons/all";
 
 const HeaderArea: React.FC = () => {
     return (
-        <Navbar collapseOnSelect expand="xl">
-            <Nav className='ml-auto d-xl-none'>
+        <Navbar collapseOnSelect expand="md">
+            <Nav className='ml-auto d-md-none'>
                 <Nav.Link href="#lookok" className='mr-4'>
                     <IoCallSharp/>
                     +94 112 123 456
@@ -14,7 +14,7 @@ const HeaderArea: React.FC = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className='ml-auto'>
-                    <Nav.Link href="#call" className='d-none d-xl-block'>
+                    <Nav.Link href="#call" className='d-none d-md-block'>
                         <IoCallSharp/>
                         +94 112 123 456
                     </Nav.Link>
@@ -22,12 +22,12 @@ const HeaderArea: React.FC = () => {
                         <FaTruck/>
                         Delivery Areas
                     </Nav.Link>
-                    <Nav.Link href="#user   ">
+                    <Nav.Link href="#user">
                         <FaUserCircle/>
                         My Account
                     </Nav.Link>
-                    <Button>Register</Button>
-                    <Button className='border-0'>Log in</Button>
+                    <Button className='my-md-2 py-md-0'>Register</Button>
+                    <Button className='border-0 my-md-2 py-md-0 ml-md-1 mt-1'>Log in</Button>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
