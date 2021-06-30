c1eecd28dfd8a31e9733d296d69ade0f3c9944fd
ovindu <ovindunambukara@gmail.com>
2021-06-30 14:26:57 +0530

    fix logo padding

--- src/components/top-nav-bar/TopNavBar.tsx
+++ src/components/top-nav-bar/TopNavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Row} from "react-bootstrap";
 import HeaderArea from "./HeaderArea";
 import LogoArea from "./LogoArea";
 
