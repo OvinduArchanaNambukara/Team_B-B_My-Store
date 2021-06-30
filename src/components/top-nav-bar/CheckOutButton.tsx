022541ad0d0faae71d60f3011809593ac487210c
ovindu <ovindunambukara@gmail.com>
2021-06-30 15:39:36 +0530

    remove checkout button in size xs

--- src/components/top-nav-bar/CheckOutButton.tsx
+++ src/components/top-nav-bar/CheckOutButton.tsx
@@ -3,7 +3,7 @@ import {Button} from "react-bootstrap";
 
 const CheckOutButton: React.FC = () => {
     return (
-        <Button className='mr-1 mr-sm-4'>Check Out</Button>
+        <Button className='mr-1 mr-sm-4 d-none d-sm-block'>Check Out</Button>
     );
 }
 
