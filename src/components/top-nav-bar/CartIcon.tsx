926f0fd3a6805b3ee54709a4f9ffefb412741bd6
ovindu <ovindunambukara@gmail.com>
2021-06-30 15:39:36 +0530

    remove checkout button in size xs

--- src/components/top-nav-bar/CartIcon.tsx
+++ src/components/top-nav-bar/CartIcon.tsx
@@ -4,7 +4,7 @@ import {Col} from "react-bootstrap";
 
 const CartIcon: React.FC = () => {
     return (
-        <Col className='text-right px-0 mr-1 mr-sm-4' sm={2} xs={4}>
+        <Col className='text-right px-0 mr-4 mr-sm-4' sm={2} xs={4}>
             <div className='count d-flex justify-content-center align-content-center'>
                 <span>0</span>
             </div>
