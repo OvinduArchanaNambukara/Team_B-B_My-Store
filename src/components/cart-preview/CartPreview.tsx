23040e6c761cb87e98211ca7341bab345716fc23
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-02 03:49:19 +0530

    cart preview item image added

--- src/components/cart-preview/CartPreview.tsx
+++ src/components/cart-preview/CartPreview.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import {Row} from "react-bootstrap";
+import {Col, Image, Row} from "react-bootstrap";
 import CartPreviewDetails from "./CartPreviewDetails";
+import CarrotImg from "../../assets/images/groceryImages/carrot.jpg";
+import DeleteIcon from "../DeleteIcon";
 
 const CartPreview: React.FC = () => {
     return (
         <Row className="cart-preview m-0 p-0">
             {/*<EmptyCartPreview/>*/}
-            <Row className="cart-items">
+            <Col xs={12} className="item-table">
+                <Row>
+                    <Col xs={4} className="p-0">
+                        <Image src={CarrotImg} fluid={true}/>
+                    </Col>
+                    <Col xs={4} className="">
+                        <label>Carrot</label><br/>
+                        <label>Qty: 1 Kg</label>
+                    </Col>
+                    <Col xs={4}>
+                        <DeleteIcon/>
+                    </Col>
+                </Row>
+            </Col>
 
-            </Row>
             <CartPreviewDetails/>
         </Row>
     );
