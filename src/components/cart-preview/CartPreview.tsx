8b46fe6c196ecd10d5f02b656c0729ac82ce0e87
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-02 15:28:16 +0530

    added cart preview items

--- src/components/cart-preview/CartPreview.tsx
+++ src/components/cart-preview/CartPreview.tsx
@@ -1,36 +1,32 @@
 import React from "react";
-import {Col, Image, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import CartPreviewDetails from "./CartPreviewDetails";
-import CarrotImg from "../../assets/images/groceryImages/carrot.jpg";
-import NumberFormat from "react-number-format";
+import CartItem from "./CartItem";
+import {CartItems} from "../../constants/constants";
+import EmptyCartPreview from "./EmptyCartPreview";
 
 const CartPreview: React.FC = () => {
     return (
-        <Row className="cart-preview m-0 p-0">
-            {/*<EmptyCartPreview/>*/}
-            <Col xs={12} className="item-table">
-                <Row>
-                    <Col xs={4} className="p-0">
-                        <Image src={CarrotImg} fluid={true}/>
-                    </Col>
-                    <Col xs={4} className="">
-                        <label>Carrot</label><br/>
-                        <label>Qty: 1 Kg</label>
-                    </Col>
-                    <Col xs={4} className="text-right">
+        <Container className="cart-preview-container px-2">
+            <Row className="cart-preview m-0 p-0">
+                {!CartItems && <EmptyCartPreview/>}
+                <Col xs={12} className="item-table">
+                    {CartItems.map((item, index) => {
+                        return (
+                            <CartItem
+                                key={index}
+                                product={item.product}
+                                quantity={item.quantity}/>
+                        );
+                    })
+                    }
+                </Col>
+                <Col className="pr-4">
+                    <CartPreviewDetails/>
+                </Col>
+            </Row>
+        </Container>
 
-                        <label className="cart-item-subtotal">
-                            <NumberFormat value={250} thousandSeparator={true} displayType='text'
-                                          prefix={'Rs. '}
-                                          decimalScale={2} fixedDecimalScale={true}
-                            />
-                        </label>
-                    </Col>
-                </Row>
-            </Col>
-
-            <CartPreviewDetails/>
-        </Row>
     );
 }
 
