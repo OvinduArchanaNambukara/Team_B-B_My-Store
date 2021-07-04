f2841494fc9c42b71c7b0557193e42b78bce68dd
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-04 23:16:53 +0530

    empty cart preview popover width fixed

--- src/components/cart-preview/CartPreview.tsx
+++ src/components/cart-preview/CartPreview.tsx
@@ -6,6 +6,7 @@ import {CartItems} from "../../constants/constants";
 import EmptyCartPreview from "./EmptyCartPreview";
 import CheckOutButton from "../common/CheckOutButton";
 import CartIcon from "./CartIcon";
+import {Simulate} from "react-dom/test-utils";
 
 const CartPreview: React.FC = () => {
     const [show, setShow] = useState(false);
@@ -27,34 +28,37 @@ const CartPreview: React.FC = () => {
                 placement="bottom-end"
                 container={ref.current}
                 rootClose={true}
+                rootCloseEvent='mousedown'
             >
-                {CartItems.length === 0 ? <EmptyCartPreview/> :
-                    <Popover id="popover-contained" className="cart-preview-popover mt-2 mt-md-3 py-2">
-                        <Container className="cart-preview-container px-2">
-                            <Row className="cart-preview m-0 p-0">
-                                <Col xs={12} className="item-table">
-                                    {CartItems.map((item, index) => {
-                                        subTotal += item.quantity * item.product.currentPrice;
-                                        return (
-                                            <CartItem
-                                                key={index}
-                                                product={item.product}
-                                                quantity={item.quantity}/>
-                                        );
-                                    })
-                                    }
-                                </Col>
-                                <Col xs={12} className="pr-4">
-                                    <CartPreviewDetails subTotal={subTotal} count={CartItems.length}/>
-                                </Col>
-                                <Col xs={12}>
-                                    <CheckOutButton
-                                        classname={"cart-checkout-btn px-5 py-1 mb-2 d-block d-sm-none float-right"}/>
-                                </Col>
-
-                            </Row>
-                        </Container>
-                    </Popover>}
+                <Popover id="popover-contained" className={CartItems.length === 0 ?
+                    `empty-cart-popover mt-2 mt-md-3 py-2 px-3` : `cart-preview-popover mt-2 mt-md-3 py-2`}>
+                    <Container className="cart-preview-container px-2">
+                        <Row className="cart-preview m-0 p-0">
+                            {CartItems.length === 0 ? <EmptyCartPreview/> :
+                                <React.Fragment>
+                                    <Col xs={12} className="item-table">
+                                        {CartItems.map((item, index) => {
+                                            subTotal += item.quantity * item.product.currentPrice;
+                                            return (
+                                                <CartItem
+                                                    key={index}
+                                                    product={item.product}
+                                                    quantity={item.quantity}/>
+                                            );
+                                        })
+                                        }
+                                    </Col>
+                                    <Col xs={12} className="pr-4">
+                                        <CartPreviewDetails subTotal={subTotal} count={CartItems.length}/>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <CheckOutButton
+                                            classname={"cart-checkout-btn px-5 py-2 mb-2 d-block d-sm-none float-right"}/>
+                                    </Col>
+                                </React.Fragment>}
+                        </Row>
+                    </Container>
+                </Popover>
             </Overlay>
         </div>
     );
