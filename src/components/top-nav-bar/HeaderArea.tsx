c3ee9ce97f6879a3e0b52d3f1779e2a2c4a17c3e
ovindu <ovindunambukara@gmail.com>
2021-07-06 23:51:34 +0530

    changed icons in top nav bar added close icon to toggle icon changed nav bar bottom border color add register login buttons hover color

--- src/components/top-nav-bar/HeaderArea.tsx
+++ src/components/top-nav-bar/HeaderArea.tsx
@@ -1,39 +1,51 @@
-import React from "react";
-import {Button, Nav, Navbar, Col} from "react-bootstrap";
-import {IoCallSharp, FaTruck, FaUserCircle} from "react-icons/all";
+import React, {useState} from "react";
+import {Button, Col, Nav, Navbar} from "react-bootstrap";
+import {CgClose, FaTruck, FiUser, IoCallOutline} from "react-icons/all";
 
 const HeaderArea: React.FC = () => {
-    return (
-        <Col xs={12} className='px-0 header-nav'>
-            <Navbar collapseOnSelect expand="md" className='py-0'>
-                <Nav className='ml-auto d-md-none'>
-                    <Nav.Link href="#lookok" className='mr-4'>
-                        <IoCallSharp/>
-                        +94 112 123 456
-                    </Nav.Link>
-                </Nav>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className='ml-auto'>
-                        <Nav.Link href="#call" className='d-none d-md-block'>
-                            <IoCallSharp/>
-                            +94 112 123 456
-                        </Nav.Link>
-                        <Nav.Link href="#delivery">
-                            <FaTruck/>
-                            Delivery Areas
-                        </Nav.Link>
-                        <Nav.Link href="#user">
-                            <FaUserCircle/>
-                            My Account
-                        </Nav.Link>
-                        <Button className='my-md-2 py-md-0'>Register</Button>
-                        <Button className='border-0 my-md-2 py-md-0 ml-md-1 mt-1'>Log in</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Col>
-    );
+  const [toggleIcon, setToggleIcon] = useState<boolean>(false);
+
+  /**
+   * when click change the toggle icon
+   * @author Ovindu
+   */
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  }
+
+  return (
+      <Col xs={12} className='px-0 header-nav'>
+        <Navbar collapseOnSelect expand="md" className='py-0'>
+          <Nav className='d-md-none'>
+            <Nav.Link href="#lookok" className='mr-4'>
+              <IoCallOutline/>
+              +94 112 123 456
+            </Nav.Link>
+          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handleToggleIcon}>
+            {toggleIcon && <CgClose size={30}/>}
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className='ml-auto'>
+              <Nav.Link href="#call" className='d-md-flex d-none pr-3'>
+                <IoCallOutline/>
+                +94 112 123 456
+              </Nav.Link>
+              <Nav.Link href="#delivery" className='pr-3'>
+                <FaTruck/>
+                Delivery Areas
+              </Nav.Link>
+              <Nav.Link href="#user" className='pr-3'>
+                <FiUser/>
+                My Account
+              </Nav.Link>
+              <Button className='my-md-2 py-md-0'>Register</Button>
+              <Button className='border-0 my-md-2 py-md-0 ml-md-1 mt-1 mb-1'>Log in</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Col>
+  );
 }
 
 export default HeaderArea;
