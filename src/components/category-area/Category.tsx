fbf1aba1f5effc236e14187e4c5a34d5476a119d
ovindu <ovindunambukara@gmail.com>
2021-07-07 01:29:09 +0530

    changed 6 categories to 5

--- src/components/category-area/Category.tsx
+++ src/components/category-area/Category.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import {Col, Image} from "react-bootstrap";
 
 type CategoryProps = {
-    name: string,
-    image: string
+  name: string,
+  image: string
 }
 
 const Category: React.FC<CategoryProps> = (props) => {
-    return (
-        <Col className='category text-center mt-2 px-1' xs={4} md={2}>
-            <div>
-                <Image src={props.image} alt={props.name} fluid={true}/>
-                <p className='m-1'>{props.name}</p>
-            </div>
-        </Col>
-    );
+  return (
+      <Col className='category text-center mt-2 px-1 px-md-2' xs={2}>
+        <div>
+          <Image src={props.image} alt={props.name} fluid={true}/>
+          <p className='m-1'>{props.name}</p>
+        </div>
+      </Col>
+  );
 }
 
 export default Category;
