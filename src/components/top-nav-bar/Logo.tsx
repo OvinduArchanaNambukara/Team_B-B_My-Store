e87bbed44f59bca0ae40acb5061f39b23eb9e203
ovindu <ovindunambukara@gmail.com>
2021-07-07 00:09:43 +0530

    added logo as clickable

--- src/components/top-nav-bar/Logo.tsx
+++ src/components/top-nav-bar/Logo.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Col} from "react-bootstrap";
 
 const Logo: React.FC = () => {
-    return (
-        <Col className='my-auto' xs={4}>
-            <p className='my-0 p-0 pl-md-4'>LOGO</p>
-        </Col>
-    );
+  return (
+      <Col className='my-auto' xs={4}>
+        <p className='my-0 p-0 pl-md-4'>
+          <span>LOGO</span>
+        </p>
+      </Col>
+  );
 }
 
 export default Logo;
