cebeeb9871eaf1c15cf439f0fed6a498aea50c3a
ovindu <ovindunambukara@gmail.com>
2021-07-07 18:05:59 +0530

    add route pathe to logo

--- src/components/top-nav-bar/Logo.tsx
+++ src/components/top-nav-bar/Logo.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {Col} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 const Logo: React.FC = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    history.push('/');
+  }
+
   return (
       <Col className='my-auto' xs={4}>
         <p className='my-0 p-0 pl-md-4'>
-          <span>LOGO</span>
+          <span onClick={handleOnClick}>LOGO</span>
         </p>
       </Col>
   );
