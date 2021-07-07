57a8cb0c35144a559956b13e5c84816bacdf80c7
ovindu <ovindunambukara@gmail.com>
2021-07-07 15:45:25 +0530

    decrease img sizes add hover bg and border color category

--- src/components/category-area/Categories.tsx
+++ src/components/category-area/Categories.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
-import fruit from "../../assets/images/pharmacy.webp";
-import furniture from "../../assets/images/electronic.webp";
-import vegetables from "../../assets/images/all.webp";
-import meat from "../../assets/images/food.webp";
-import grocery from "../../assets/images/grocery.webp";
+import pharmacy from "../../assets/images/categories/pharmacy.webp";
+import electronic from "../../assets/images/categories/electronic.webp";
+import all from "../../assets/images/categories/all.webp";
+import food from "../../assets/images/categories/food.webp";
+import grocery from "../../assets/images/categories/grocery.webp";
 import Category from "./Category";
 import {categoryTypes} from "../../types/types";
 
 const temp: categoryTypes[] = [
-  {image: fruit, name: 'Fruits'},
-  {image: furniture, name: 'Furniture'},
-  {image: vegetables, name: 'Vegetables'},
-  {image: meat, name: 'Meat'},
-  {image: grocery, name: 'Grocery'}
+  {image: all, name: 'All'},
+  {image: grocery, name: 'Grocery'},
+  {image: pharmacy, name: 'Pharmacy'},
+  {image: food, name: 'Food'},
+  {image: electronic, name: 'Electronic'}
 ];
 
 const Categories: React.FC = () => {
 
-    return (
-        <Col className='px-1 px-sm-0 text-center' xs={12}>
-            <Row className='justify-content-center'>
-                {
-                    temp.map((category: categoryTypes, index: number) => {
-                        return (
-                            <Category key={index} name={category.name} image={category.image}/>
-                        );
-                    })
-                }
-            </Row>
-        </Col>
-    );
+  return (
+      <Col className='px-1 px-sm-0 text-center' xs={12}>
+        <Row className='justify-content-center'>
+          {
+            temp.map((category: categoryTypes, index: number) => {
+              return (
+                  <Category key={index} name={category.name} image={category.image}/>
+              );
+            })
+          }
+        </Row>
+      </Col>
+  );
 }
 
 export default Categories;
