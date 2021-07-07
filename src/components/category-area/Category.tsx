200117ea3cf3e7adc1b850d91e3ebb072eb1a9d4
ovindu <ovindunambukara@gmail.com>
2021-07-07 16:36:47 +0530

    add style when click category

--- src/components/category-area/Category.tsx
+++ src/components/category-area/Category.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {CSSProperties, useState} from "react";
 import {Col, Image} from "react-bootstrap";
+import {categoryClickStyles} from "../../custom-styles/custom-category-click-styles";
 
 type CategoryProps = {
   name: string,
@@ -7,9 +8,26 @@ type CategoryProps = {
 }
 
 const Category: React.FC<CategoryProps> = (props) => {
+  const [styles, setStyles] = useState<CSSProperties>({});
+  const [isClicked, setIsClicked] = useState(true);
+
+  /**
+   * when clicked set custom styles to div
+   * @author Ovindu
+   */
+  const handleOnClick = () => {
+    if (isClicked) {
+      setStyles(categoryClickStyles);
+      setIsClicked(false);
+    } else {
+      setStyles({});
+      setIsClicked(true);
+    }
+  }
+
   return (
       <Col className='category text-center mt-2 px-1 px-md-2' xs={2}>
-        <div>
+        <div onClick={handleOnClick} style={styles}>
           <Image src={props.image} alt={props.name} fluid={true}/>
           <p className='m-1'>{props.name}</p>
         </div>
