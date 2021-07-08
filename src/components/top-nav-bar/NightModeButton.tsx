42fc74839b50018d7831e36521800ea346eb56ce
ovindu <ovindunambukara@gmail.com>
2021-07-08 18:54:55 +0530

    add responsive for button

--- src/components/top-nav-bar/NightModeButton.tsx
+++ src/components/top-nav-bar/NightModeButton.tsx
@@ -3,9 +3,13 @@ import Toggle from "react-toggle";
 import {CgSun, IoMoonOutline} from "react-icons/all"
 import {iconColors} from "../../custom-styles/custom-nightmode-icons";
 
-const NightModeButton: React.FC = () => {
+type NightModeButton = {
+  customClassName: string
+}
+
+const NightModeButton: React.FC<NightModeButton> = (prop) => {
   return (
-      <div className='align-items-center d-flex pr-md-3 pl-4 pl-md-0'>
+      <div className={prop.customClassName}>
         <Toggle
             defaultChecked={false}
             icons={{
