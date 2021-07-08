7945c93bedae9e6802eda1a3c05763bbae6f88ec
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-07-08 10:58:27 +0530

    Add update Btn Change product font sizes change product Btn border radius fix checkout table

--- src/components/cart-table/CheckoutHeader.tsx
+++ src/components/cart-table/CheckoutHeader.tsx
@@ -3,8 +3,8 @@ import {Col} from "react-bootstrap";
 
 const CheckoutHeader:React.FC = () => {
     return(
-        <Col md={8} xl={10} lg={9} sm={6} className='px-0 checkOut-label text-md-left text-center'>
-            <h3>CheckOut</h3>
+        <Col md={8} xl={10} lg={9} sm={6} className='px-0 pt-2 checkOut-label text-md-left text-center'>
+            <h4 className='mb-0'>CheckOut Page</h4>
         </Col>
     )
 }
