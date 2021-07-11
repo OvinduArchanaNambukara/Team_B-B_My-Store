d194bf75fa0d53ccb479e90be6df598406f4cdbe
ovindu <ovindunambukara@gmail.com>
2021-07-11 18:31:44 +0530

    add redux store cartItems to table

--- src/components/cart-table/DeleteIcon.tsx
+++ src/components/cart-table/DeleteIcon.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {BiTrashAlt} from "react-icons/all";
+import {useDispatch} from "react-redux";
+import {deleteFromCart} from "../../store/actions/CartActions";
 
-const DeleteIcon: React.FC = () => {
+type DeleteIconProps = {
+  index: number
+}
 
-    return (
-        <BiTrashAlt/>
-    )
+const DeleteIcon: React.FC<DeleteIconProps> = (props) => {
+  const {index} = props;
+  const dispatch = useDispatch();
+
+  /**
+   * when click delete that item in redux store cartItems[]
+   * @author Ovindu
+   */
+  const handleOnDeleteClick = () => {
+    dispatch(deleteFromCart(index));
+  }
+
+  return (
+      <BiTrashAlt onClick={handleOnDeleteClick}/>
+  )
 }
 
 export default DeleteIcon;
