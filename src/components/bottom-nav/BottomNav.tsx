aebca21538d328ed0631afd4892f329b20514b29
ovindu <ovindunambukara@gmail.com>
2021-07-21 03:13:07 +0530

    fix routes in bottom nav bar

--- src/components/bottom-nav/BottomNav.tsx
+++ src/components/bottom-nav/BottomNav.tsx
@@ -1,42 +1,44 @@
 import React from "react";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import {NavLink} from "react-router-dom"
+import {Container, Nav, Navbar, NavDropdown, NavLink} from "react-bootstrap";
+import {Link, useHistory} from "react-router-dom"
 
 const BottomNav: React.FC = () => {
+  const history = useHistory();
+
   return (
       <Container fluid={true}>
         <Navbar className="bottom-nav-bar px-0 py-3 border-bottom" expand="sm">
           <NavDropdown title="Categories" id="basic-nav-dropdown" className="bottom-nav-dropdown mr-md-4 ml-md-4 px-1">
-            <NavDropdown.Item>
-              <NavLink to={'/home/All'} className='p-0 text-decoration-none'>All</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/All')}>
+              All
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Grocery'} className='p-0 text-decoration-none'>Grocery</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Grocery')}>
+              Grocery
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Pharmacy'} className='p-0 text-decoration-none'>Pharmacy</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Pharmacy')}>
+              Pharmacy
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Food'} className='p-0 text-decoration-none'>Food</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Food')}>
+              Food
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Electronic'} className='p-0 text-decoration-none'>Electronic</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Electronic')}>
+              Electronic
             </NavDropdown.Item>
           </NavDropdown>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav" className="bottom-nav-collapse">
             <Nav className="bottom-nav mr-auto ">
               <Nav.Link>
-                <NavLink to='/'>Home</NavLink>
+                <Link to='/'>Home</Link>
               </Nav.Link>
               <Nav.Link>
-                <NavLink to='/faq'>FAQ</NavLink>
+                <Link to='/faq'>FAQ</Link>
               </Nav.Link>
               <Nav.Link>
-                <NavLink to='/aboutus'>About us</NavLink>
+                <Link to='/aboutus'>About us</Link>
               </Nav.Link>
               <Nav.Link>
-                <NavLink to='/contactus'>Contact us</NavLink>
+                <Link to='/contactus'>Contact us</Link>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
